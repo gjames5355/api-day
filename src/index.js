38b45c486bc9276e432e346cf27cd2ef6c891ba5
Garrett James <garrettjames55@gmail.com>
2020-03-02 07:57:08 -0500

    updated shopping-list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import $ from 'jquery';
-
+import store from './store';
 import 'normalize.css';
 import './index.css';
-
 import shoppingList from './shopping-list';
-
 import api from './api';
 
 const main = function () {
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 
   shoppingList.bindEventListeners();
   shoppingList.render();
